983c1b324e48b83a2f56b781c84577a018b3b76e
YuheTian02 <118496619+YuheTian02@users.noreply.github.com>
2024-02-18 16:35:25 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -531,5 +531,5 @@
   </div>
 
   <div class="footer-link" style="text-align: center; position: fixed; bottom: 0; width: 100%; background-color: #f1f1f1; padding: 10px 0;">
-  <a href="https://github.com/JasonDai1219/DSC-106-Project-3/blob/main/README.md" target="_blank">Here is a detailed description to our work.</a>
+    <a href="https://github.com/JasonDai1219/DSC-106-Project-3/blob/main/README.md" target="_blank">Here is a detailed description to our work.</a>
   </div>
