d7ad68e6645990960f60d6c12b65c461b5c6a913
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2024-02-21 20:54:04 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -284,7 +284,7 @@
                  .duration(200)
                  .style("opacity", .9);
             tooltip.html(`Country: China<br>Year: ${selectedYear}<br>Type: ${d.category}<br>Amount: ${d.value + "%"}`)
-                 .style("left", (event.pageX) + "px")
+                 .style("left", (event.pageX - 100) + "px")
                  .style("top", (event.pageY - 28) + "px");
         })
         .on("mouseout", () => {
